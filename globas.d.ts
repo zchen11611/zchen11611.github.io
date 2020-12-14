f3f44d7e6ba4bd0ea997abb9e9382c6f7e70d447
zchen11611 <a104366259@outlook.com>
2020-12-14 08:26:11 +0000

    update by actions

---
+++ globas.d.ts
@@ -0,0 +1,4 @@
+
+interface JQuery {
+  circleProgress(...args: any[]): JQuery;
+}
---
+++ js/loadCss.js
@@ -0,0 +1,1 @@
+define(()=>{const head=document.head;return(function(url,callback){return new Promise((res,rej)=>{const link=document.createElement("link");link.type="text/css",link.rel="stylesheet",link.id="loadCss-"+Date.now(),link.href=url,link.onload=()=>{res(link),callback&&callback(link)},link.onerror=rej,head.appendChild(link)})})});
